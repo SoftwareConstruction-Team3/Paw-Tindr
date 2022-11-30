b6b28d55d45f0e1798bd873e93c17a710b78e93c
macamarilloelizondo <43419781+macamarilloelizondo@users.noreply.github.com>
2022-11-30 16:32:14 -0700

    Update chat_view
    Update chat view with one message on the scree with logo.

--- paw_tindr/lib/views/chat_view.dart
+++ paw_tindr/lib/views/chat_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+/*
 class ChatView extends StatefulWidget {
   const ChatView({Key? key}) : super(key: key);
 
@@ -13,6 +13,61 @@ class _ChatViewState extends State<ChatView> {
     return const Scaffold(
       body: Center(
         child: Text('This is where you will view your chats'),
+      ),
+    );
+  }
+}
+*/
+class ChatView extends StatefulWidget {
+  const ChatView({Key? key}) : super(key: key);
+
+  @override
+  State<ChatView> createState() => _ChatViewState();
+}
+
+class _ChatViewState extends State<ChatView> {
+  Image displayLogo() {
+    return const Image(
+      image: AssetImage('assets/images/header_logo.png'),
+      width: 400.0,
+      alignment: FractionalOffset.topCenter,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 1,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.orangeAccent[200],
+            title: //const Text('Paw-Tindr'),
+                const Image(
+              image: AssetImage('assets/images/header_logo.png'),
+              width: 150.0,
+              alignment: FractionalOffset.topCenter,
+            ),
+            bottom: const TabBar(tabs: [
+              //Tab(icon: Icon(Icons.person)),
+              Tab(icon: Icon(Icons.message)),
+              //Tab(icon: Icon(Icons.settings)),
+            ]),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              ListTile(
+                title: const Text('Daisy',
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0)),
+              subtitle: const Text('Hello how are you?'),
+              leading: Icon(
+                Icons.message,
+                color: Colors.orangeAccent[200],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
